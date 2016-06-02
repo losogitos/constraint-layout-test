134d773ce7ea0bcc6dbf01ded8ffe535c02e2872
Łukasz Ciupa <lukasz.ciupa@stxnext.pl>
2016-06-02 11:33:16 +0200

    Percentage activity button added, percentage activity craeted.

--- app/src/main/java/com/stxnext/constraintlayoutsample/MainActivity.java
+++ app/src/main/java/com/stxnext/constraintlayoutsample/MainActivity.java
@@ -2,12 +2,27 @@ package com.stxnext.constraintlayoutsample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getName();
+
+    private Button percentActivityButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        percentActivityButton = (Button) findViewById(R.id.percent_activity);
+        percentActivityButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "Percentage activity clicked.");
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/stxnext/constraintlayoutsample/PercentageActivity.java
@@ -0,0 +1,8 @@
+package com.stxnext.constraintlayoutsample;
+
+/**
+ * Created by lukasz on 02.06.16.
+ */
+
+public class PercentageActivity {
+}
