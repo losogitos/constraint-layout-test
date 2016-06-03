c5bd24996eaa9553c4e981b76e4497e3ac945896
Łukasz Ciupa <lukasz.ciupa@stxnext.pl>
2016-06-03 15:19:39 +0200

    Align activity.

---
+++ app/src/main/java/com/stxnext/constraintlayoutsample/AlignActivity.java
@@ -0,0 +1,18 @@
+package com.stxnext.constraintlayoutsample;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by lukasz on 03.06.16.
+ */
+
+public class AlignActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.align_activity);
+    }
+}
